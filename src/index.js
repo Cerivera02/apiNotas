aa15e022e950d5bc7a1afe3f396c7929f0f5b96b
Carlos Eduardo <cerivera2002@gmail.com>
2024-01-16 18:27:39 -0700

    Se agrega CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');  // Importa cors
 const { createDato, getDatos, updateDato, deleteDato } = require('./controllers');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());  // Habilita CORS
 app.use(bodyParser.json());
 
 app.post('/api/datos', createDato);
@@ -15,4 +17,24 @@ app.delete('/api/datos/:id', deleteDato);
 app.listen(port, () => {
   console.log(`Servidor Iniciado correctamente :)`);
 });
+
+
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const { createDato, getDatos, updateDato, deleteDato } = require('./controllers');
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(bodyParser.json());
+
+// app.post('/api/datos', createDato);
+// app.get('/api/datos', getDatos);
+// app.put('/api/datos/:id', updateDato);
+// app.delete('/api/datos/:id', deleteDato);
+
+// app.listen(port, () => {
+//   console.log(`Servidor Iniciado correctamente :)`);
+// });
 
